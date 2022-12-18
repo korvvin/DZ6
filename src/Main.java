f0031fe919737b253eb3b7910e109d217a0a8dcd
zykov <zykov.da01@gmail.com>
2022-12-18 23:16:40 +0300

    dz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 2000;
+        task1(a);
+        boolean clientOS = false; //false(0)-android , true(1)-IOS
+        short age = 2020;
+        task2(clientOS, age);
+        int deliveryDistance = 15;
+        task3(deliveryDistance);
+
+    }
+
+    public static void task1(int a) {
+        if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
+            System.out.println(a + " год - високосный");
+        } else {
+            System.out.println(a + " год - не високосный");
+        }
+    }
+
+    public static void task2(boolean clientOS, short age) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS && age < currentYear) {
+            System.out.println("Установите упрощеную версию приложения для iOS по ссылке");
+        } else if (clientOS && age == currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (!clientOS && age == currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (!clientOS && age < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task3(int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            System.out.println("потребуется 1 день");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("потребуется 2 дня");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("потребуется 3 дня");
+        } else {
+            System.out.println("доставки нет");
+        }
     }
 }
